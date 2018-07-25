d41cb3473c90136e69fa81c8f0dd22913d2551c3
Sumaya Ahmad <sum_sa.f198@hotmail.com>
2018-07-25 13:44:02 +0400

    Add project files.

---
+++ FitnessCenterApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitnessCenterApp.Startup))]
+namespace FitnessCenterApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
